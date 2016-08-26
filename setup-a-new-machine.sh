00775d5c2150cf2fe3d3bc83838e073f4a68881f
Patrick <patrick@pidgypost.com>
2016-08-26 19:06:23 -0400

    cleanup xcode CLT comments
    added opening and closing comment headers as well as description. moved credits to header.

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -18,11 +18,17 @@
 ##############################
 
 
-#################
+#########################################
 #### OPEN - XCode Command Line Tools ####
-#################
+#########################################
+# Desctiption:
+## This snippet will walk the user through installing XCode CLT if it is not installed
 # Credits:
 ## https://github.com/alrra/dotfiles/blob/ff123ca9b9b/os/os_x/installs/install_xcode.sh
+## https://github.com/alrra/dotfiles/issues/13
+## https://github.com/alrra/dotfiles/issues/10
+
+
 
 if ! xcode-select --print-path &> /dev/null; then
 
@@ -42,7 +48,6 @@ if ! xcode-select --print-path &> /dev/null; then
 
     # Point the `xcode-select` developer directory to
     # the appropriate directory from within `Xcode.app`
-    # https://github.com/alrra/dotfiles/issues/13
 
     sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
     print_result $? 'Make "xcode-select" developer directory point to Xcode'
@@ -50,14 +55,14 @@ if ! xcode-select --print-path &> /dev/null; then
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     # Prompt user to agree to the terms of the Xcode license
-    # https://github.com/alrra/dotfiles/issues/10
 
     sudo xcodebuild -license
     print_result $? 'Agree with the XCode Command Line Tools licence'
 
 fi
-###
-##############################################################################################################
+##########################################
+#### CLOSE - XCode Command Line Tools ####
+##########################################
 
 
 
